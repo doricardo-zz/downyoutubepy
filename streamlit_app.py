066aa7221a691497e70e7fbe87458642c249a8d3
Ricardo Andrade <email@doricardo.com>
2022-04-17 06:23:40 -0300

    update files

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 from src.downyoutubepy import DownYoutubePy
 import streamlit as st
 import pandas as pd
+import os
 #import time
 
 # App Title and Mode
@@ -13,11 +14,15 @@ url = st.text_input('Youtube Url', key='url', value='')
 a = st.button('Download', key='download')
 if option and url:
     app = DownYoutubePy()
+    save_path = 'D:\\Google Drive\\Mp3'
     if option == 'audio':
-        app.ydl_opts = {'format': 'bestaudio/best', 'postprocessors': [{ 'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '128'}],}
+        app.ydl_opts = {'format': 'bestaudio/best', 'postprocessors': [{ 'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '128'}],
+        'outtmpl': save_path + '/%(title)s.%(ext)s',}
     elif option == 'video':
-        app.ydl_opts = {'format': 'bestvideo+bestaudio', 'postprocessors': [{ 'key': 'FFmpegVideoConvertor', 'preferedformat': 'mp4'}],}
+        app.ydl_opts = {'format': 'bestvideo+bestaudio', 'postprocessors': [{ 'key': 'FFmpegVideoConvertor', 'preferedformat': 'mp4'}],
+        'outtmpl': save_path + '/%(title)s.%(ext)s',}
 
     app.url = url
     app.typefile = option
     app.download()
+    st.text('Finish')
